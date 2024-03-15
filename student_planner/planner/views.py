fc360718cdecad5fbf10afe92bedea75d132818f
eddthebedd <jeongef@bc.edu>
2024-03-15 13:44:36 -0400

    Added views on views.py

--- student_planner/planner/views.py
+++ student_planner/planner/views.py
@@ -1,9 +1,21 @@
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
-# Create your views here.
+# Custom Login View
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    redirect_authenticated_user = True
 
-def index(request):
-    return render(request, 'planner/login.html')
+# Landing Page for Students after Login
+class StudentLandingPageView(LoginRequiredMixin, TemplateView):
+    template_name = 'landing_student.html'
 
-def planner(request):
-    return render(request, 'planner/landing_student.html')
+# Course Plans View
+class CoursePlansView(LoginRequiredMixin, TemplateView):
+    template_name = 'course_plans.html'
+
+# Explore Major View
+class ExploreMajorView(LoginRequiredMixin, TemplateView):
+    template_name = 'explore_major.html'
