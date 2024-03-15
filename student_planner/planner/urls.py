8650949c687d962fcf1eb91a345b83f6afad54ee
eddthebedd <jeongef@bc.edu>
2024-03-15 13:50:47 -0400

    Added new view paths on urls.py

--- student_planner/planner/urls.py
+++ student_planner/planner/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('planner/', views.planner, name='planner'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('student-landing/', StudentLandingPageView.as_view(), name='landing_student'),
+    path('course-plans/', CoursePlansView.as_view(), name='course_plans'),
+    path('explore-major/', ExploreMajorView.as_view(), name='explore_major'),
 ]
