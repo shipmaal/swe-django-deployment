fbe2b14b07cb5399dc8fad8ea67954faeffe88d0
eddthebedd <jeongef@bc.edu>
2024-03-17 16:22:05 -0400

    Updated admin.py with information from models.py

--- student_planner/planner/admin.py
+++ student_planner/planner/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Student, Admin, Planner, Program, Course
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Admin)
+admin.site.register(Planner)
+admin.site.register(Program)
+admin.site.register(Course)
