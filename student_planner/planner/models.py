efaf96044dd191c1ab53effaae78adf1d28a7970
lhawks202 <lhawks202@gmail.com>
2024-03-19 01:49:27 -0400

    Created Database

--- student_planner/planner/models.py
+++ student_planner/planner/models.py
@@ -27,11 +27,13 @@ class Choice(models.Model):
 class Student(models.Model):
     def __str__(self):
         return self.eagle_id
-    eagle_id = models.IntegerField(max_length=8)
+    
+    eagle_id = models.CharField(max_length=8)
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
-    email = models.EmailField(max_length = 254, primary_key)
-    class_year = models.IntegerField(max_length=4)
+    email = models.EmailField(max_length = 254)
+    class_year = models.CharField(max_length=4)
+    end_semester = models.CharField(max_length=6, default="Spring")
 
     class College(models.TextChoices):
         MCAS = "MCAS", _("Morrissey College of Arts and Sciences")
@@ -45,20 +47,29 @@ class Student(models.Model):
     )
 
 class Admin(models.Model):
-   def __str__(self):
+    def __str__(self):
         return self.eagle_id
-    eagle_id = models.IntegerField(max_length=8)
+    
+    eagle_id = models.CharField(max_length=8)
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
-    email = models.EmailField(max_length = 254, primary_key)
-    class_year = models.IntegerField(max_length=4) 
+    email = models.EmailField(max_length = 254)
+    class_year = models.CharField(max_length=4) 
 
 class Planner(models.Model):
-    student = models.ForignKey(Student, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
     
 
 
 class Program(models.Model):
-
+    title = models.CharField(max_length=4)
+    mum_credits = models.IntegerField()
+    def __str__(self):
+        return self.title
 
 class Course(models.Model):
+    class_code = models.CharField(max_length=4)
+    associated_programs = models.ManyToManyField(Program)
+    mum_credits = models.IntegerField()
+    def __str__(self):
+        return self.class_code
