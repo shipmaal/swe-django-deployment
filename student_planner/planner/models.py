7775ec1788615ed79e49dc6e3f99d173e31407cf
shipmaal <100368040+shipmaal@users.noreply.github.com>
2024-03-19 13:24:37 -0400

    Add Major and Minor models, update Student model relationships

--- student_planner/planner/models.py
+++ student_planner/planner/models.py
@@ -4,19 +4,34 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
+class Major(models.Model):
+    title = models.CharField(max_length=4)
+    mum_credits = models.IntegerField()
+    def __str__(self):
+        return self.title
+    
+    
+
+class Minor(models.Model):
+    title = models.CharField(max_length=4)
+    mum_credits = models.IntegerField()
+    def __str__(self):
+        return self.title
+
+
 class Student(models.Model):
     def __str__(self):
         return self.eagle_id
     eagle_id = models.CharField(max_length=8)
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
-    email = models.EmailField(max_length = 254, primary_key=True)
+    email = models.EmailField(max_length=254, primary_key=True)
     class_year = models.CharField(max_length=4)
     end_semester = models.CharField(max_length=6, default="Spring")
-    major_one = models.ForeignKey(Major, default=None)
-    major_two = models.ForeignKey(Major, default=None)
-    minor_one = models.ForeignKey(Major, default=None)
-    minor_two = models.ForeignKey(Major, default=None)
+    major_one = models.ForeignKey(Major, default=None, on_delete=models.CASCADE, related_name="major_one")
+    major_two = models.ForeignKey(Major, default=None, on_delete=models.CASCADE, related_name="major_two")
+    minor_one = models.ForeignKey(Minor, default=None, on_delete=models.CASCADE, related_name="minor_one")
+    minor_two = models.ForeignKey(Minor, default=None, on_delete=models.CASCADE, related_name="minor_two")
 
     class College(models.TextChoices):
         MCAS = "MCAS", _("Morrissey College of Arts and Sciences")
@@ -43,18 +58,6 @@ class Planner(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     
 
-
-class Major(models.Model):
-    title = models.CharField(max_length=4)
-    mum_credits = models.IntegerField()
-    def __str__(self):
-        return self.title
-
-class Minor(models.Model):
-    title = models.CharField(max_length=4)
-    mum_credits = models.IntegerField()
-    def __str__(self):
-        return self.title
 
 class Course(models.Model):
     class_code = models.CharField(max_length=4)
