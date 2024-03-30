fc482199a8c1d2b340cb0a87dc82e74433c210ef
shipmaal <100368040+shipmaal@users.noreply.github.com>
2024-03-30 17:52:59 -0400

    Refactor form widgets and update background styling

--- student_planner/student_planner/forms.py
+++ student_planner/student_planner/forms.py
@@ -5,6 +5,21 @@ from planner.models import Student, Major, Minor, Advisor
 year_choices = [(r,r) for r in range(2025, datetime.date.today().year+4)]
 current_year = datetime.date.today().year
 
+def create_widget(field_type, **kwargs):
+    attrs = {'class': 'form-control'}
+    if kwargs.get('disabled'):
+        attrs['disabled'] = 'disabled'
+    if field_type == 'TextInput':
+        return forms.TextInput(attrs=attrs)
+    elif field_type == 'EmailInput':
+        return forms.EmailInput(attrs=attrs)
+    elif field_type == 'Select':
+        return forms.Select(choices=kwargs.get('choices', []), attrs=attrs)
+    elif field_type == 'HiddenInput':
+        return forms.HiddenInput()
+    else:
+        return None
+
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
@@ -23,18 +38,18 @@ class StudentForm(forms.ModelForm):
             'minor_two',
         ]
         widgets = {
-            'eagle_id': forms.TextInput(attrs={'class': 'form-control',}),
-            'first_name': forms.TextInput(attrs={'class': 'form-control', 'disabled': 'disabled'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-control', 'disabled': 'disabled'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'disabled': 'disabled'}),
-            'class_year': forms.Select(choices=year_choices, attrs={'class': 'form-control'}),
-            'end_semester': forms.Select(attrs={'class': 'form-control'}),
-            'college': forms.Select(attrs={'class': 'form-control'}),
-            'advisor': forms.HiddenInput(),
-            'major_one': forms.Select(attrs={'class': 'form-control'}),
-            'major_two': forms.Select(attrs={'class': 'form-control'}),
-            'minor_one': forms.Select(attrs={'class': 'form-control'}),
-            'minor_two': forms.Select(attrs={'class': 'form-control'}),
+            'eagle_id': create_widget('TextInput'),
+            'first_name': create_widget('TextInput', disabled=True),
+            'last_name': create_widget('TextInput', disabled=True),
+            'email': create_widget('EmailInput', disabled=True),
+            'class_year': create_widget('Select', choices=year_choices),
+            'end_semester': create_widget('Select'),
+            'college': create_widget('Select'),
+            'advisor': create_widget('HiddenInput'),
+            'major_one': create_widget('Select'),
+            'major_two': create_widget('Select'),
+            'minor_one': create_widget('Select'),
+            'minor_two': create_widget('Select'),
         }
     major_one = forms.ModelChoiceField(queryset=Major.objects.all(), 
                                        empty_label="Select a Major", 
@@ -51,10 +66,10 @@ class StudentForm(forms.ModelForm):
 
 class StudentAccountForm(StudentForm):
     class Meta(StudentForm.Meta):
-        fields = StudentForm.Meta.fields + ['advisor']
+        fields = StudentForm.Meta.fields
         widgets = {**StudentForm.Meta.widgets, 
-                   'advisor': forms.Select(attrs={'class': 'form-control'}),
-                   'eagle_id': forms.TextInput(attrs={'class': 'form-control', 'disabled': 'disabled'})}
+                   'advisor': create_widget('Select', choices=[]),
+                   'eagle_id': create_widget('TextInput', disabled=True),}
         advisor = forms.ModelChoiceField(queryset=Advisor.objects.all(),
                                         empty_label="Select an Advisor",
                                         required=False)
