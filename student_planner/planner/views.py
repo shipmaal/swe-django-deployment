65cbf5d188d9c835e67412755450c6cd8e4a7d40
lhawks202 <lhawks202@gmail.com>
2024-04-02 14:09:06 -0400

    Added check for no planner

--- student_planner/planner/views.py
+++ student_planner/planner/views.py
@@ -1,7 +1,7 @@
 from typing import Any
 from django.views.generic import TemplateView
 from django.shortcuts import redirect
-from planner.models import Student, Course, Planner
+from planner.models import Student, Course, Planner, Subject, Semester
 from .api import PlanningCoursesAPI
 import dataclasses
 import json
@@ -30,33 +30,38 @@ class StudentLandingPageView(TemplateView):
             return super().dispatch(request, *args, **kwargs)
         
 
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         student = Student.objects.get(email=self.request.user.email)
-        planners = student.planner_set.all()
-        # planners = Planner.objects.filter(student=student)
-        sem_1 = planners[0].sem_one
-        enrolled_courses = [
-            sem_1.class_one,
-            sem_1.class_two,
-            sem_1.class_three,
-            sem_1.class_four,
-            sem_1.class_five,
-            sem_1.class_six
-        ]
-        # data = self.api.get_courses_by_code('CSCI1074')
-        context['data'] = data
-        data_dict = dataclasses.asdict(data[0])
-        data = self.api.get_all_courses()
-        subject = data[0].subjectArea
-        course = data[0].course
-        print(course['id'])
-        print(course['subjectAreaId'])
-        print(subject['id'])
-        print(subject['shortName'])
-        print(subject['longName'])
-        context['data_dict'] = json.dumps(data_dict)
-        context['enrolled_courses'] = enrolled_courses
+        planners = Planner.objects.filter(student=student)
+        if planners:
+            sem_1 = planners[0].sem_one
+            enrolled_courses = [
+                sem_1.class_one,
+                sem_1.class_two,
+                sem_1.class_three,
+                sem_1.class_four,
+                sem_1.class_five,
+                sem_1.class_six
+            ]
+            '''
+            data = self.api.get_courses_by_code('CSCI1074')
+            context['data'] = data
+            data_dict = dataclasses.asdict(data[0])
+            data = self.api.get_all_courses()
+            subject = data[0].subjectArea
+            course = data[0].course
+            print(course['id'])
+            print(course['subjectAreaId'])
+            print(subject['id'])
+            print(subject['shortName'])
+            print(subject['longName'])
+            '''
+            context['data_dict'] = json.dumps(data_dict)
+            context['enrolled_courses'] = enrolled_courses
+            context['planners'] = planners
+            '''
         return context
 
 # Course Plans View
